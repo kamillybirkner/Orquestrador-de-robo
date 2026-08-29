89340c745b40bf97884d1beac680f7825da2b03b
Kamilly Birkner <kamillybirkner@gmail.com>
2026-08-29 20:47:09 -0300

    feat: cria model, service e componente inicial da tela de robos

---
+++ web/src/app/pages/robots-list/robots-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-robots-list',
+  imports: [],
+  templateUrl: './robots-list.html',
+  styleUrl: './robots-list.scss',
+})
+export class RobotsList {
+
+}
